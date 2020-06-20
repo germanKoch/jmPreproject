8720d75cc12fb5b991c34eb530970664e3d05aad
German <gemikgerman@gmail.com>
2020-06-20 19:29:25 +0300

    second task completed

--- 1.1.3/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ 1.1.3/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,44 +1,53 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+
+    private SessionFactory sessionFactory;
+
+    public UserServiceImpl() {
+        this.sessionFactory = Util.getSessionFactory();
+    }
+
     @Override
     public void createUsersTable() {
-        getUserDaoJDBC().createUsersTable();
+        getUserDaoHibernate().createUsersTable();
     }
 
     @Override
     public void dropUsersTable() {
-        getUserDaoJDBC().dropUsersTable();
+        getUserDaoHibernate().dropUsersTable();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        getUserDaoJDBC().saveUser(name, lastName, age);
+        getUserDaoHibernate().saveUser(name, lastName, age);
     }
 
     @Override
     public void removeUserById(long id) {
-        getUserDaoJDBC().removeUserById(id);
+        getUserDaoHibernate().removeUserById(id);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return getUserDaoJDBC().getAllUsers();
+        return getUserDaoHibernate().getAllUsers();
     }
 
     @Override
     public void cleanUsersTable() {
-        getUserDaoJDBC().cleanUsersTable();
+        getUserDaoHibernate().cleanUsersTable();
     }
 
-    private static UserDaoJDBCImpl getUserDaoJDBC() {
-        return new UserDaoJDBCImpl(Util.getMysqlConnection());
+    private static UserDaoHibernateImpl getUserDaoHibernate() {
+        return new UserDaoHibernateImpl(Util.getSessionFactory().openSession());
     }
 }
--- 1.1.3/src/main/java/jm/task/core/jdbc/util/Util.java
+++ 1.1.3/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,32 +1,46 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    public static Connection getMysqlConnection() {
-        try {
-            DriverManager.registerDriver((Driver) Class.forName("com.mysql.jdbc.Driver").newInstance());
+    private static SessionFactory sessionFactory;
 
-            StringBuilder url = new StringBuilder();
-
-            url.
-                    append("jdbc:mysql://").        //db type
-                    append("localhost:").           //host name
-                    append("3306/").                //port
-                    append("preproj1?").          //db name
-                    append("user=german&").          //login
-                    append("password=Germanpaulivalerman123");       //password
-
-            System.out.println("URL: " + url + "\n");
-
-            Connection connection = DriverManager.getConnection(url.toString());
-            return connection;
-        } catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new IllegalStateException();
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            sessionFactory = createSessionFactory();
         }
+        return sessionFactory;
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    private static Configuration getMySqlConfiguration() {
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/preproj1");
+        configuration.setProperty("hibernate.connection.username", "german");
+        configuration.setProperty("hibernate.connection.password", "Germanpaulivalerman123");
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+        return configuration;
+    }
+
+    private static SessionFactory createSessionFactory() {
+        Configuration configuration = getMySqlConfiguration();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+        builder.applySettings(configuration.getProperties());
+        ServiceRegistry serviceRegistry = builder.build();
+        return configuration.buildSessionFactory(serviceRegistry);
     }
 }
