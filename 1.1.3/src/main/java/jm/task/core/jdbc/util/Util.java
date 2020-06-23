8179e107f788ab2898821df7dce2a9cc82b82e31
German <gemikgerman@gmail.com>
2020-06-23 17:25:32 +0300

    change password

--- 1.1.3/src/main/java/jm/task/core/jdbc/util/Util.java
+++ 1.1.3/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -30,7 +30,7 @@ public class Util {
         configuration.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
         configuration.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/preproj1");
         configuration.setProperty("hibernate.connection.username", "german");
-        configuration.setProperty("hibernate.connection.password", "Germanpaulivalerman123");
+        configuration.setProperty("hibernate.connection.password", "german");
         configuration.setProperty("hibernate.show_sql", "true");
         configuration.setProperty("hibernate.hbm2ddl.auto", "update");
         return configuration;
